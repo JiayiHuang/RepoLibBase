d1b64414da6e97b150ce2b3e6c7cef8e1092a6cd
HuangJiyun <jiyun@haiwaifenqi.com>
2018-08-09 17:41:34 +0800

    测试 子模块更改

---
+++ src/main/java/com/hjy/lib/TestUtils.java
@@ -0,0 +1,13 @@
+package com.hjy.lib;
+
+/**
+ * @author HJY
+ * @date 2018/08/09/17:35
+ * <pre>
+ *     desc   : 文件描述
+ *     version: 当前版本号
+ * </pre>
+ */
+public class TestUtils {
+    // TODO(Hjy): 2018/8/9 测试 submodule 的使用
+}
